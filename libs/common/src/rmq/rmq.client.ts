1b8599b2871b390a6c1250287945d6fda2a0001e
Peev,Plamen P.K <plamen.peev@student.fontys.nl>
2024-04-26 19:18:35 +0200

    Cleanup

--- libs/common/src/rmq/rmq.client.ts
+++ libs/common/src/rmq/rmq.client.ts
@@ -1,11 +1,12 @@
 import { ClientProxy } from '@nestjs/microservices';
 import { NexPayload } from '../dto/nex-payload';
 import { lastValueFrom } from 'rxjs';
+import { PN } from '../constants';
 
 export class RmqClient {
   constructor(private client: ClientProxy) {}
 
-  async emit(pattern: string, payload: NexPayload<any>) {
+  async emit(pattern: PN, payload: NexPayload<any>) {
     await lastValueFrom(this.client.emit(pattern, payload));
   }
 }
--- libs/common/src/tcp/tcp.client.ts
+++ libs/common/src/tcp/tcp.client.ts
@@ -1,11 +1,12 @@
 import { ClientProxy } from '@nestjs/microservices';
 import { NexPayload } from '../dto/nex-payload';
 import { lastValueFrom } from 'rxjs';
+import { PN } from '../constants';
 
 export class TcpClient {
   constructor(private client: ClientProxy) {}
 
-  async send(pattern: string, payload: NexPayload<any>) {
+  async send(pattern: PN, payload: NexPayload<any>) {
     return await lastValueFrom(this.client.send(pattern, payload));
   }
 }
