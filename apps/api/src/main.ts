98a679172534b68bb4c627d0349515f302160bfc
Peev,Plamen P.K <plamen.peev@student.fontys.nl>
2024-04-26 15:37:33 +0200

    Cookie parser.

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,11 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
 import { ApiModule } from './api.module';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(ApiModule);
   const configService = app.get(ConfigService);
   app.useGlobalPipes(new ValidationPipe());
+  app.use(cookieParser());
   await app.listen(configService.get('API_PORT'));
 }
 bootstrap();
