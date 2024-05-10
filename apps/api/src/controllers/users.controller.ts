af62330a06179bfbb42a81d55776b3b480fa8137
Peev,Plamen P.K <plamen.peev@student.fontys.nl>
2024-05-10 11:37:00 +0200

    Cleanup

--- apps/api/src/controllers/users.controller.ts
+++ apps/api/src/controllers/users.controller.ts
@@ -1,5 +1,5 @@
 import { PN, TCP_USERS } from '@app/common/constants';
-import { Body, Controller, Get, Inject, UseGuards } from '@nestjs/common';
+import { Body, Controller, Inject, Post, UseGuards } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { AuthGuard } from '../middleware/auth.guard';
 import { CurrentUser } from '@app/common/decorators';
@@ -11,7 +11,7 @@ import { firstValueFrom } from 'rxjs';
 export class UsersController {
   constructor(@Inject(TCP_USERS) private tcpOrders: ClientProxy) {}
 
-  @Get('chefs')
+  @Post('chefs')
   async getChefs(@CurrentUser() user: UserSession, @Body() data: ReadChefsRequest) {
     return await firstValueFrom(this.tcpOrders.send(PN.get_chefs, new NexPayload(data, user)));
   }
